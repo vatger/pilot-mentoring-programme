74a2f1ec08c6220b2eec5f9983784b6b75ff1f9f
Jacob Koglin <jlkoglin@icloud.com>
2025-10-28 20:31:25 +0100

    More Backgroundimages

--- src/components/BackgroundProvider.tsx
+++ src/components/BackgroundProvider.tsx
@@ -41,7 +41,7 @@ export default function BackgroundProvider({
       const isMobile = window.innerWidth < 768;
 
       if (!isMobile) {
-            const imageFiles = ['1.PNG', '2.PNG', '3.PNG', '4.PNG', '5.PNG', '6.PNG', '7.PNG', '8.PNG', '9.PNG'];
+            const imageFiles = ['1.PNG', '2.PNG', '3.PNG', '4.PNG', '5.PNG', '6.PNG', '7.PNG', '8.PNG', '9.PNG', '10.PNG', '11.PNG', '12.PNG', '13.PNG'];
             const imageUrls = imageFiles.map(file => `/images/${file}`);
         // choose one random image for this page load (do not rotate)
           try {
